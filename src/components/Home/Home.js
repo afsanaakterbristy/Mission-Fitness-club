c2d05a6c4f23750c567c1773d08a66b5e985b10c
afsanaakterbristy <afsanaakter.bristy28@gmail.com>
2022-09-28 23:49:04 +0600

    added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -23,9 +23,44 @@ const Home = () => {
                   missions.map(mission=>(<Cart mission={mission} key={mission.id}></Cart>))  
                 }
             </div>
-            <div className='left-side'>
-                <h2>okk2</h2>
-            </div>
+                <div className='left-side'>
+                    <div className='left-cart'>
+                         <img src="https://images.unsplash.com/photo-1548690312-e3b507d8c110?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fGd5bXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60" alt="" />
+                        <div>
+                            <h3 className='cart-details'>Name:Akter</h3>
+                            <p className='cart-details'>Dhaka,Bangladesh</p>
+                        </div>
+                    </div>
+                    <div className='right-cart'>
+                       <h4>Weight:53kg</h4>
+                       <h4>Height:5.4ft</h4>
+                    </div>
+                  <div>
+                        <h3 className='break-title'>Add A Break</h3>
+                        <div className='exercise-time'>
+                        <button className='break-btn'>30s</button>
+                        <button className='break-btn'>40s</button>
+                        <button className='break-btn'>50s</button>
+                        <button className='break-btn'>60s</button>
+                        <button className='break-btn'>70s</button>
+                        </div>
+                        
+                </div>
+                <div>
+                    <h3 className='break-title'>Exercise Details</h3>
+                        <div className='exercise-time'>
+                        <h4>Exercise time</h4>
+                        <h4>300</h4>
+                        </div>
+                        <div className='exercise-time'>
+                        <h4>Break time</h4>
+                        <h4>300</h4>
+                        </div>
+                </div>
+               <button className='btn'>Activity Completed</button>
+
+                </div>
+               
             </div>
             </>
     );

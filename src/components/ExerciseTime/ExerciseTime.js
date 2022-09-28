420fd183eb788e20b98d8a9cb18cb15d5e565656
afsanaakterbristy <afsanaakter.bristy28@gmail.com>
2022-09-29 02:01:13 +0600

    added

--- src/components/ExerciseTime/ExerciseTime.js
+++ src/components/ExerciseTime/ExerciseTime.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ExerciseTime.css'
 
-const ExerciseTime = () => {
+const ExerciseTime = ({exerciseTime }) => {
+    const [breakTime, setBreakTime] = useState(0);
+     const handleBreakTime = (time) => {
+         setBreakTime(time);
+    }
+    //console.log(exerciseTime);
+    let totalTime = 0;
+    for (const mission of exerciseTime) {
+        totalTime  = totalTime  + mission.time;
+    }
+
     return (
+ 
         <div>
              <div className='left-cart'>
                          <img src="https://images.unsplash.com/photo-1548690312-e3b507d8c110?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fGd5bXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60" alt="" />
@@ -18,11 +29,13 @@ const ExerciseTime = () => {
                   <div>
                         <h3 className='break-title'>Add A Break</h3>
                         <div className='exercise-time'>
-                        <button className='break-btn'>30s</button>
-                        <button className='break-btn'>40s</button>
-                        <button className='break-btn'>50s</button>
-                        <button className='break-btn'>60s</button>
-                        <button className='break-btn'>70s</button>
+                        
+                        <button onClick={()=>handleBreakTime(20)}  className='break-btn'>20 <span>s</span></button>
+                        <button onClick={()=>handleBreakTime(30)}  className='break-btn'>30 <span>s</span></button>
+                        <button onClick={()=>handleBreakTime(40)}  className='break-btn'>40 <span>s</span></button>
+                        <button onClick={()=>handleBreakTime(50)}  className='break-btn'>50 <span>s</span></button>
+                        <button onClick={()=>handleBreakTime(60)}  className='break-btn'>60 <span>s</span></button>
+                        
                         </div>
                         
                 </div>
@@ -30,11 +43,11 @@ const ExerciseTime = () => {
                     <h3 className='break-title'>Exercise Details</h3>
                         <div className='exercise-time'>
                         <h4>Exercise time</h4>
-                        <h4>300</h4>
+                        <h4>{totalTime}</h4>
                         </div>
                         <div className='exercise-time'>
                         <h4>Break time</h4>
-                        <h4>300</h4>
+                        <h4>{breakTime}</h4>
                         </div>
                 </div>
                <button className='btn'>Activity Completed</button>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,7 +7,8 @@ import ExerciseTime from '../ExerciseTime/ExerciseTime';
 
 const Home = () => {
     const [missions, setMissions] = useState([]);
-    const [exerciseTime,setExerciseTime]=useState([])
+    const [exerciseTime, setExerciseTime] = useState([]);
+   
     useEffect(() => {
         fetch('missions.json')
         .then(res => res.json())
@@ -19,6 +20,7 @@ const Home = () => {
         const newCart = [...exerciseTime, mission];
         setExerciseTime(newCart)
     }
+   
 
     return (
         <>
@@ -33,41 +35,8 @@ const Home = () => {
             </div>
                 <div className='left-side'>
 
-                    <ExerciseTime exerciseTime={exerciseTime}></ExerciseTime>
-                    {/* <div className='left-cart'>
-                         <img src="https://images.unsplash.com/photo-1548690312-e3b507d8c110?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fGd5bXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60" alt="" />
-                        <div>
-                            <h3 className='cart-details'>Name:Akter</h3>
-                            <p className='cart-details'>Dhaka,Bangladesh</p>
-                        </div>
-                    </div>
-                    <div className='right-cart'>
-                       <h4>Weight:53kg</h4>
-                       <h4>Height:5.4ft</h4>
-                    </div>
-                  <div>
-                        <h3 className='break-title'>Add A Break</h3>
-                        <div className='exercise-time'>
-                        <button className='break-btn'>30s</button>
-                        <button className='break-btn'>40s</button>
-                        <button className='break-btn'>50s</button>
-                        <button className='break-btn'>60s</button>
-                        <button className='break-btn'>70s</button>
-                        </div>
-                        
-                </div>
-                <div>
-                    <h3 className='break-title'>Exercise Details</h3>
-                        <div className='exercise-time'>
-                        <h4>Exercise time</h4>
-                        <h4>300</h4>
-                        </div>
-                        <div className='exercise-time'>
-                        <h4>Break time</h4>
-                        <h4>300</h4>
-                        </div>
-                </div>
-               <button className='btn'>Activity Completed</button> */}
+                    <ExerciseTime exerciseTime={exerciseTime} ></ExerciseTime>
+                    
 
                 </div>
                
